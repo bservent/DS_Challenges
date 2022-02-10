f415416ebad14ecba0ffb51220048c2064060a7a
Brittany Servent <bservent@gmail.com>
2022-02-10 09:22:19 -0800

    map in react for li

--- react.js
+++ react.js
@@ -16,3 +16,28 @@ ReactDOM.render(<App />, document.getElementById("root"));
 
 console.log(App()) */
 
+/**
+  Challenge: Display all users to the browser
+  Small Hint: Use array map with react key
+  
+  Solution: https://codepen.io/angelo_jin/pen/wvreMpZ
+  Video for reference: https://youtu.be/VzNNjNmbXpY
+**/
+
+/* const users = [
+  { name: "John Doe", id: 1 },
+  { name: "Jane Doe", id: 2 },
+  { name: "Billy Doe", id: 3 }
+];
+
+const userItems = users.map((user) => <li key={user.id}>{user.name}</li>);
+function App() {
+  return (
+    <>
+      <h3>User names</h3>
+      <ul>{userItems}</ul>
+    </>
+  );
+}
+
+ReactDOM.render(<App />, document.getElementById("root")); */
